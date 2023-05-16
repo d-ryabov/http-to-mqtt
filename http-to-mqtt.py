09891543ecb6ade6f36b7274de77441f6a61e2f3
Dmitriy Ryabov <ryabov.dmitriy@icloud.com>
2023-05-16 20:29:44 +0300

    Add version and some logging

--- http-to-mqtt.py
+++ http-to-mqtt.py
@@ -9,7 +9,7 @@ import os
 import paho.mqtt.client as mqtt
 import re
 import sys
-import traceback
+from _version import __version__
 
 
 class LocalData(object):
@@ -31,13 +31,16 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         mqtt_client.publish('{0}/{1}'.format(topic_id, value),LocalData.records[topic_id][value])
       self.send_response(200)
     else:
+      logger.debug('HTTP: Incorrect path')
       self.send_response(403)
     self.end_headers()
 
   def do_GET(self):
     if re.search('/api/get/*', self.path):
       topic_id = self.path.split('api/get')[1]
+      logger.debug('HTTP: Recieved GET at {0}. Payload: {1}'.format(self.path, data))
       if topic_id in LocalData.records:
+        logger.debug('LocalData: Data found')
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
@@ -47,15 +50,12 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         logger.debug('LocalData: Get {0} from {1}'.format(data,topic_id))
         self.wfile.write(data)
       else:
+        logger.debug('LocalData: Data not found')
         self.send_response(404, 'Not Found: record does not exist')
     else:
+      logger.debug('HTTP: Incorrect path')
       self.send_response(403)
     self.end_headers()
-
-parser = argparse.ArgumentParser(prog=os.path.basename(__file__), description='Simple HTTP server for receiving data and then sending to MQTT.')
-parser.add_argument('--port', metavar='port', type=int, default=8000,
-          help='HTTP server port')
-pargs = parser.parse_args()
 
 def get_logger():
   logname = '{0}.log'.format(os.path.basename(__file__).split('.')[0])
@@ -85,21 +85,30 @@ def get_mqtt_client():
   try:
     mqtt_client.connect(broker_address)
     logger.debug('MQTT: Connected')
-  except BaseException:
+  except BaseException as e:
     logger.exception('MQTT: An error occured during the connection. Application will shut down')
     sys.exit(1)
 
   return mqtt_client
 
+parser = argparse.ArgumentParser(prog=os.path.basename(__file__), description='Simple HTTP server for receiving data and then sending to MQTT.')
+parser.add_argument('--version', action='version',
+                    version='%(prog)s {version}'.format(version=__version__))
+pargs = parser.parse_args()
+
+
 if __name__ == '__main__':
   logger = get_logger()
+  logger.debug('APP: Started')
   mqtt_client = get_mqtt_client()
   server = HTTPServer(('', pargs.port), HTTPRequestHandler)
-  logger.debug('HTTP: Starting httpd...')
+
   try:
+    logger.debug('HTTP: Starting httpd...')
     server.serve_forever()
   except KeyboardInterrupt:
-    logger.debug('Interrupted from keyboard')
+    logger.debug('APP: Interrupted from keyboard')
   finally:
     logger.debug('HTTP: Stopping httpd...')
     server.server_close()
+    logger.debug('APP: Finished')
